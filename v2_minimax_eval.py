d0d6c592062a3c15ff5063fb43abcb73753978cc
Max Schemenauer (Temp) <mschemen@calpoly.edu>
2024-09-04 16:27:39 -0700

    correctly evaluates stalemate and repetition as draw

--- v2_minimax_eval.py
+++ v2_minimax_eval.py
@@ -13,6 +13,7 @@ piece_values = {1: 1, 2: 3, 3: 3, 4: 5, 5: 9, 6: 100}
 class v2_Minimax_Eval:
     def __init__(self, game):
         self.game = game
+        self.position_counts = {}
 
     def move(self):
         board = self.game.board
@@ -20,35 +21,58 @@ class v2_Minimax_Eval:
         random.shuffle(legal_moves)
         if len(legal_moves) == 0:
             return self.game.check_game_state()
-        maximizing = board.turn
-        # if not maximizing:
-        #     # print("color black")
+
+        is_white = board.turn
         best_eval = float('-inf')
         best_move = legal_moves[0]
+        self.update_position_counts(board)  # update to get move opponent played
+
         for move in legal_moves:
             board.push(move)
-            eval = self.evaluate_board(board, maximizing)
-            if not maximizing:
-                eval = -eval
-            # print(move, eval)
+            self.update_position_counts(board)  # update to get move we're testing
+            eval = self.evaluate_board(board, is_white)
+            self.update_position_counts(board, decrement=True)  # remove tested move
             if eval > best_eval:
-                # print("best eval was", best_eval, "is now", eval)
-                # print("best move is", best_move)
                 best_move = move
                 best_eval = eval
             board.pop()
 
         board.push(best_move)
+        self.update_position_counts(board)  # update with move bot played
         return self.game.check_game_state()
 
-    def evaluate_board(self, board, maximizing):
+    def evaluate_board(self, board, is_white):
         score = 0
+        if board.is_checkmate():
+            return float('inf')
+        if board.is_stalemate() or self.is_potential_threefold_repetition(board):
+            score = 0
+
         for piece_type in chess.PIECE_TYPES:
             for square in board.pieces(piece_type, chess.WHITE):
                 score += piece_values[piece_type]
             for square in board.pieces(piece_type, chess.BLACK):
                 score -= piece_values[piece_type]
-            if board.is_checkmate():
-                val = float('inf') if maximizing else float('-inf')
-                score += val
+        if not is_white:
+            score = -score
         return score
+
+    def is_potential_threefold_repetition(self, board):
+        fen = board.fen()
+        return self.position_counts.get(fen, 0) >= 2
+
+    def update_position_counts(self, board, decrement=False):
+        fen = board.fen()
+        if decrement:
+            if fen in self.position_counts:
+                self.position_counts[fen] -= 1
+                if self.position_counts[fen] == 0:
+                    del self.position_counts[fen]
+        else:
+            if fen in self.position_counts:
+                self.position_counts[fen] += 1
+            else:
+                self.position_counts[fen] = 1
+
+    def reset(self):
+        self.position_counts = {}

162b14dd4d3fbc517fdcdaf217ea2ffc60b3a3c7
Max Schemenauer (Temp) <mschemen@calpoly.edu>
2024-09-04 16:24:56 -0700

    logging now includes list of moves in uci format

--- simulate.py
+++ simulate.py
@@ -1,8 +1,11 @@
 import os
 
+import chess
+import chess.pgn
 from game import Game
 import time
 import cProfile
+import io
 
 from engines.v1_random import v1_Random
 from simulation_renderer import SimulationRenderer
@@ -16,9 +19,9 @@ class Simulate:
         self.renderer = None
 
     @staticmethod
-    def log_results(bot1_name, bot2_name, data, win_count):
+    def log_results(bot1_name, bot2_name, data, win_count, uci_moves_list):
         col_width = max(len(bot1_name), len(bot2_name)) + 2
-        header = f"{' Result':<{col_width}}| {bot1_name + ' Color':<{col_width}}\n"
+        header = f"{' Result':<{col_width}}| {'Winning Color':<{col_width}}\n"
         current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         folder = 'simulation_results'
         if not os.path.exists(folder):
@@ -28,8 +31,10 @@ class Simulate:
             log_file.write(f"Simulation started at: {current_time}\n")
             log_file.write(f"{bot1_name} vs. {bot2_name}\n\n")
             log_file.write(header)
+            i = 0
             for result in data:
-                log_file.write(f"{result[0]:<{col_width}}|{result[1]:<{col_width}}\n")
+                log_file.write(f"{result[0]:<{col_width}}|{result[1]:<{col_width}}| {uci_moves_list[i]}\n")
+                i+=1
             # Write win count summary
             log_file.write(f"\n{bot1_name}: {win_count[bot1_name]}\n")
             log_file.write(f"{bot2_name}: {win_count[bot2_name]}\n")
@@ -44,12 +49,13 @@ class Simulate:
         while True:
             game_ended = current_bot()
             if game_ended:
+                # helping me detect likelihood a bot will draw
+                print(current_bot_name, "had last move")
                 winner = current_bot_name if self.game.board.is_checkmate() else "Draw"
                 if visual:
                     self.renderer.game_ended = True
                     self.renderer.update_screen()
                     self.renderer.game_ended = False
-                self.game.restart()
                 return winner
             # Swap bots and their names for the next turn
             current_bot, next_bot = next_bot, current_bot
@@ -58,41 +64,53 @@ class Simulate:
                 self.renderer.update_screen()
 
     def run_simulations(self, num_games, bot1, bot2, visual=False):
+        # initialize bot information
         bot1_name = bot1.__class__.__name__
         bot2_name = bot2.__class__.__name__
         if bot1_name == bot2_name:
             bot2_name += "(1)"
         bot1_move = bot1.move
         bot2_move = bot2.move
-
         if visual:  # sets up renderer to show the game
             self.renderer = SimulationRenderer(self.game)
-
         data = []
+        uci_moves_list = []
         win_count = {bot1_name: 0, bot2_name: 0, "Draw": 0}
         bot1_white = True
         for i in range(num_games):
-            if visual:
+            if visual:  # update color of bot in simulation
                 self.renderer.bot1_white = bot1_white
             if bot1_white:
-                color = "White"
+                color = {bot1_name: "White", bot2_name: "Black", "Draw": ""}
             else:
-                color = "Black"
+                color = {bot1_name: "Black", bot2_name: "White", "Draw": ""}
+            # play game
             winner = self.start_game(bot1_move, bot2_move, bot1_name, bot2_name, visual, bot1_white)
+            # record stats, (winner, color, list of moves)
             win_count[winner] += 1
-            data.append([" " + winner, " " + color])
+            data.append([" " + winner, " " + color[winner]])
+            game = chess.pgn.Game.from_board(self.game.board)
+            uci_moves = []
+            for move in game.mainline_moves():
+                uci_moves.append(move.uci())
+            uci_moves_list.append(uci_moves)
+            # reset bots and game, switch bots color
+            bot1.reset()
+            bot2.reset()
+            self.game.restart()
             bot1_white = not bot1_white
-        self.log_results(bot1_name, bot2_name, data, win_count)
+        # finally, record all the results in a file
+        self.log_results(bot1_name, bot2_name, data, win_count, uci_moves_list)
 
 
 if __name__ == "__main__":
     simulate = Simulate()
     mode = 'nvisual'
+    visual = mode == 'visual'
     bot_1 = v1_Random(simulate.game)
     bot_2 = v2_Minimax_Eval(simulate.game)
-    visual = mode == 'visual'
-    profiler = cProfile.Profile()
-    profiler.enable()
-    simulate.run_simulations(100, bot_1, bot_2, visual=visual)
-    profiler.disable()
-    profiler.print_stats(sort='time')
+    # profiler = cProfile.Profile()
+    # profiler.enable()
+    simulate.run_simulations(10, bot_1, bot_2, visual=visual)
+    # profiler.disable()
+    # profiler.print_stats(sort='time')

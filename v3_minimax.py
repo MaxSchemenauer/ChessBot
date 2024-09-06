54777e209d871346b98497c8eed8628fb58fccef
Max Schemenauer (Temp) <mschemen@calpoly.edu>
2024-09-06 15:33:28 -0700

    more efficient, may be a bug with having different color pieces

--- v3_minimax.py
+++ v3_minimax.py
@@ -21,25 +21,21 @@ class v3_Minimax:
         self.best_eval = None
         self.game = game
         self.position_counts = {}
+        self.time_and_positions = []
+        self.positions_evaluated = 0
 
     def move(self):
         """
         begins search. sets up board and gets legal moves
         """
+        #print("\nstarting search")
+        start = time.time()
         board = self.game.board
         self.update_position_counts(board)  # update to get opponents moves
-
+        self.positions_evaluated = 0
         self.best_move = None
-        # TODO; check time on eval mate vs search mate
-        start = time.time()
-        print("starting search")
-        # profiler = cProfile.Profile()
-        # profiler.enable()
-        self.best_eval = self.search(board, depth=4, alpha=float('-inf'), beta=float('inf'))
-        end = time.time()
-        print("move took", end - start, "seconds")
-        # profiler.disable()
-        # profiler.print_stats(sort='time')
+
+        self.best_eval = self.search(board, ply_remaining=4, ply_from_root=0, alpha=float('-inf'), beta=float('inf'))
 
         if self.best_move is None:  # if no move happens to be found, use a random one
             moves = list(board.legal_moves)
@@ -48,51 +44,49 @@ class v3_Minimax:
         else:
             board.push(self.best_move)
 
-        print(self.best_move, self.best_eval)
         self.update_position_counts(board)  # update for this move
-
+        end = time.time()
+        self.time_and_positions.append((f'{self.best_move.uci()}, {self.best_eval}', round((end - start), 3), f": {self.positions_evaluated} positions evaluated"))
+        print(self.time_and_positions[-1])
         return self.game.check_game_state()
 
-    def search(self, board, depth, alpha, beta):
-        if depth == 0:  # evaluate
-            return self.evaluate(board)
-
+    def search(self, board, ply_remaining, ply_from_root, alpha, beta):
         moves = board.legal_moves
-
         if not moves:
             if board.is_checkmate():  # checkmate is the worst outcome
+                self.positions_evaluated += 1  # early eval
                 return float('-inf')
-        # if board.is_stalemate() or self.is_potential_threefold_repetition(board):
-        #     return -100  # is a draw
+            else:  # stalemate, or another form of draw
+                return 0
+        if ply_from_root > 0:
+            if self.is_potential_threefold_repetition(board):
+                self.positions_evaluated += 1  # early eval
+                return -66
+        if ply_remaining == 0:  # evaluate
+            self.positions_evaluated += 1  # early eval
+            return self.evaluate(board)
 
         best_eval = float('-inf')
         for move in moves:
             board.push(move)
-            eval = -self.search(board, depth - 1, -beta, -alpha)
+            eval = -self.search(board, ply_remaining - 1, ply_from_root + 1, -beta, -alpha)
             board.pop()
             if eval > best_eval:
                 best_eval = eval
-                if depth == 4:
-                    self.best_move = move
+                if ply_remaining == 4:
+                    self.best_move = move  # assigns best move at top level
+            if ply_remaining == 4:
+                print(move, eval)
             alpha = max(alpha, eval)
             if alpha >= beta:
                 break  # Alpha-beta pruning
 
         return best_eval
 
-    def evaluate(self, board):
-        score = 0
-        # if board.is_checkmate():  # checkmate for is the best outcome
-        #     print("eval mate")
-        #     return float('-inf')
-        # if board.is_stalemate() or self.is_potential_threefold_repetition(board):
-        #     return 0  # is a draw, regardless of other heuristics
+    @staticmethod
+    def evaluate(board):
         score = 0
         piece_map = board.piece_map()
-
-        if board.is_stalemate() or self.is_potential_threefold_repetition(board):
-            print("threefold detected")
-            return -1  # is a draw
 
         for square, piece in piece_map.items():
             # Add or subtract piece value depending on color
@@ -101,19 +95,21 @@ class v3_Minimax:
                 score += value
             else:
                 score -= value
-
         return score if board.turn else -score
 
     def is_potential_threefold_repetition(self, board):
-        fen = board.fen()
-        return self.position_counts.get(fen, 0) >= 2
+        return board.board_fen() in self.position_counts
 
     def update_position_counts(self, board):
-        fen = board.fen()
+        fen = board.board_fen()
         if fen in self.position_counts:
             self.position_counts[fen] += 1
         else:
             self.position_counts[fen] = 1
 
     def reset(self):
+        times = [time for _, time, _ in self.time_and_positions]
+        print("average time per move:", sum(times) / len(times))
         self.position_counts = {}
+        self.time_and_positions = []
+        self.positions_evaluated = 0
